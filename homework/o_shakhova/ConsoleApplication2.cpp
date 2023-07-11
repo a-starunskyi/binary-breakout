60a47b3b659dd27b74eea59989bdb176a0639bc1
O1esya <olesya.shakhova03@gmail.com>
2023-07-11 19:28:36 +0300

    Create ConsoleApplication2.cpp

---
+++ homework/o_shakhova/ConsoleApplication2.cpp
@@ -0,0 +1,25 @@
+﻿// ConsoleApplication2.cpp : This file includs function "main". 
+
+// Preprocessor directive that includes header iostream
+#include <iostream>
+
+// Start of program: function block main()
+int main()
+{
+	// std - is an abbreviation for standard
+	// Getting console output and end-line from std;
+	using std:: cout;
+	using std:: endl;
+	cout << " Hello World!" << endl;
+	cout << " Olesia Shakhova " << endl;
+	cout << " 08.03.2003 " << endl;
+	cout << " 2003 - 2023" << endl;
+
+	// Return a value to the OS
+	return 0;
+
+}
+
+// Run the program: CTRL+F5 or menu "Debug" > "Run without debugging"
+// Debug program: F5 or "Debug" menu > "Start Debugging"
+
