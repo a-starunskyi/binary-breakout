600d1e1f94eff39804663a1eb37c6d4414028013
Andrii Starunskyi <Starunskiy.as@gmail.com>
2023-07-15 23:52:13 +0200

    add lesson5 homework

---
+++ lesson5/task1_ConditionalExecutionUsingIfElse/main.cpp
@@ -0,0 +1,14 @@
+// Problem: Write a program that takes two numbers from the user and displays which one is greater.
+
+#include<iostream>
+using namespace std;
+
+int main() {
+	int num1, num2;
+
+	// TODO: Take input from the user
+
+	// TODO: Implement the if ... else logic to determine and display the greater number
+
+	return 0;
+}
