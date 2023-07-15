15813f96385cce155f04a025b5a79606462241c0
Andrii Starunskyi <Starunskiy.as@gmail.com>
2023-07-15 22:58:20 +0200

    added lesson1 homework

---
+++ lesson1/main.cpp
@@ -0,0 +1,5 @@
+// Write a simple “Hello World” like program.
+// The program should output : your name, surname, date of birth and age.
+// To output “age” use numbers of current year and birth year and subtraction(-).For example : 2023 - 1990
+// Add comments to all parts of program what it does.
+// Commit the code to your branch.
