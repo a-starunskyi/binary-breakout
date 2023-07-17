54a83d8cb93622551cdcc226690bb25ac057f2e7
Bohdan <bodiavon48@gmail.com>
2023-07-17 11:47:44 +0300

    Finished lesson1

---
+++ lesson1/lesson1.cpp
@@ -0,0 +1,15 @@
+//Preprocessor directive that is used with the header "main"
+#include <iostream>
+
+//main function that starts the whole program 
+int main()
+{
+	/*writes text to the screen*/
+	std::cout << "Bohdan Dunaiev" << std::endl;
+	
+	/*writee age of birth with the help of subtraction*/
+	std::cout << 2023 - 2001 << std::endl;
+
+	/*returns value to the OS, helps to check if the program has done its funciton successfully*/
+	return 0;
+}
