d36acec90225234fab4aff233875e39ed56b37d4
O1esya <olesya.shakhova03@gmail.com>
2023-07-18 20:34:24 +0300

    Finished lesson 2
    Corrected version

---
+++ homework/o_shakhova/lesson 2/Defining&UsingConstants1/Defining&UsingConstants1/Defining&UsingConstants1.cpp
@@ -0,0 +1,41 @@
+﻿// Defining and Using Constants.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+/*The program should prompt the user to input the radius of the circle.
+Use the constant PI(3.14159) in your calculation.*/
+
+#include <iostream>
+
+int main()
+{
+    using std::cout;
+    using std::endl;
+    using std::cin;
+
+    cout << "Let's calculate the area of a circle!" << endl;
+    cout << "Formula the area of circle is A = pi r^2" << endl;
+
+    double radius = 0.0; // Initialize valiable for radius
+    const double pi = 3.14; // Using the constant of pi
+    cout << " The value of constant pi is: " << pi << endl;
+
+    cout << "Write the radius of the circle: ";
+    cin >> radius;
+
+    double result = pi * (radius * radius);
+    cout << " The area of the circle is: " << result << endl;
+
+    return 0;
+}
+
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
