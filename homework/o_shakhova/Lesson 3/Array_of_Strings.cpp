3cc8043267f1028588ed528cbe6d1f035c3b7809
O1esya <olesya.shakhova03@gmail.com>
2023-07-19 14:35:21 +0200

    Finished lesson 3

---
+++ homework/o_shakhova/Lesson 3/Array_of_Strings.cpp
@@ -0,0 +1,21 @@
+﻿// Array_of_Strings.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Write a program that declares an array of strings with values "Apple", "Banana", "Cherry". Print the first string from the array
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    // Declare and initialize an array of strings
+    string fruits[] = { "Apple", "Banana", "Cherry" };
+
+    //Print the first string from the array
+    cout << "First string: " << fruits[0] << endl;
+
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
---
+++ homework/o_shakhova/Lesson 3/Swapping_Without_If_Statements.cpp
@@ -0,0 +1,24 @@
+﻿// Swapping_Without_If_Statements.cpp: This file contains the 'main' function. Program execution begins and ends there.
+/*Write a program that declares an integer array of size 2 with the values 5 and 10.
+Swap the values of the two elements using a temporary variable and print the result.*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int myArray[2] = { 5, 10 };
+
+	// Swap the values using a temporary variable
+	int temp = myArray[0];
+	myArray[0] = myArray[1];
+	myArray[1] = temp;
+
+	//Print the result after swapping
+	cout << "After swapping: " << myArray[0] << " " << myArray[1] << endl;
+
+	return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
