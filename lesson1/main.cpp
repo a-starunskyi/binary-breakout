b068690310776a55053a3dee52031d00239f596b
Dia1234567 <62073943+Dia1234567@users.noreply.github.com>
2023-07-20 18:16:41 +0300

    Finished lesson 1

---
+++ lesson1/main.cpp
@@ -0,0 +1,15 @@
+// Preprocessor directive that includes header iostream
+#include <iostream>
+
+// Start of your program: function block main()
+int main()
+{
+	/* Write to the screen */
+	std::cout << "Diana Harnyk" << std::endl;
+
+	/*Write age of birth with help of subtraction*/
+	std::cout << "2023 - 2003" << std::endl;
+
+	// Return a value to the OS
+	return 0;
+}
