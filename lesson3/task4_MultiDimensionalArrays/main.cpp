ad71bb5e8407dd812b90ce09231ad6eff2a23705
Serhii <serosa0408@gmail.com>
2023-07-24 12:46:27 +0200

    LESSON 3 FIXED VERSION

--- lesson3/task4_MultiDimensionalArrays/main.cpp
+++ lesson3/task4_MultiDimensionalArrays/main.cpp
@@ -4,7 +4,7 @@ int main() {
     // TODO:
     int integerArray[2][2] = {{ 1, 2 }, { 3, 4 }};
 
-    std::cout << "Element at row 1 and column 2: " << integerArray[1][0] << std::endl;
+    std::cout << "Element at row 1 and column 2: " << integerArray[0][1] << std::endl;
 
     return 0;
 }
