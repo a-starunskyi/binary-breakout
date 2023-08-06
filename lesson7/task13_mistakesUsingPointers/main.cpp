673a2837b61b494ebd0507635be7d855accfb571
Mark-Karpenko <138462694+Mark-Karpenko@users.noreply.github.com>
2023-08-06 16:22:06 +0200

    lesson 7.2

--- lesson7/task13_mistakesUsingPointers/main.cpp
+++ lesson7/task13_mistakesUsingPointers/main.cpp
@@ -1,1 +1,12 @@
 // Problem 13: Write a program that has a common pointer mistake and explain the mistake in comments.
+
+#include <iostream>
+
+int main() {
+    int* ptr; // Declaring a pointer without initializing it.
+
+    // Attempting to dereference an uninitialized pointer (Common Pointer Mistake)
+    std::cout << "Value of *ptr: " << *ptr << std::endl;
+
+    return 0;
+}
--- lesson7/task4_pointersStoreAddresses/main.cpp
+++ lesson7/task4_pointersStoreAddresses/main.cpp
@@ -1,2 +1,16 @@
 // Problem 4: Write a program that declares an integer, a float and pointers to them. 
 // Assign the addresses of the variables to the respective pointers.
+
+#include<iostream>;
+
+int main()
+{
+	int num = 10;
+	float floNum = 5.55;
+
+	int* pointerToNum = &num;
+	float* pointerToFloat = &floNum;
+
+	std::cout << "Pointer to int : " << *pointerToNum << std::endl;
+	std::cout << "Pointer to float : " << *pointerToFloat << std::endl;
+}
