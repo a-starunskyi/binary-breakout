4090654bfc2cd053026e27f8f76fee56152c788e
Serhii <serosa0408@gmail.com>
2023-08-06 15:37:07 +0200

    lesson5 fixed task8

--- lesson5/task8_WhileLoop/main.cpp
+++ lesson5/task8_WhileLoop/main.cpp
@@ -4,19 +4,29 @@
 
 
 int main() {
-	int number1;
-	int number2 = 1;
-	std::cout << "enter a not negative number: " << std::endl;
-	std::cin >> number1;
-	while (number2 <= number1) {
-		number2++;
+	int num = 0;
+	std::cout << "enter a num: ";
+	std::cin >> num;
+
+	int factorial = 1;
+	int i = 1;
+
+
+	while (i <= num) {
+		factorial *= i;
+		i++;
 	}
 
-	std::cout << "factorial of " << number1 << " is: " << number2 << std::endl;
-	// TODO: Take input from the user
+	std::cout << "factorial of: " << num << " is " << factorial << std::endl;
 
+
+
+
+
+
+
+	// TODO: Take input from the user
 	// TODO: Implement a while loop to calculate and display the factorial of the number
 
 	return 0;
 }
-// shoulder shrugs
